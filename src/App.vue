ec61f9153427c8d0383e62ad41fc82e9ffd6a1bf
oliversablove <osablove@gmail.com>
2019-08-06 15:36:18 -0400

    Add Login Page Styling

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,12 @@
 body {
   margin: 0;
   padding: 0;
-  background-color: #f3f3f3;
+  background-color: #f7f7f7;
   font-family: 'Roboto', Helvetica, sans-serif;
+}
+
+#app {
+  width: 100vw;
+  min-height: 100vh;
 }
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,20 @@
 <template>
   <div class='login-page'>
-    <h3>Snap<span>shot</span></h3>
-    <div class='form-group'>
-      <input type='text' v-model='email' placeholder='email'/>
-      <input type='password' v-model='password' placeholder='password'/>
-      <button class='login-btn'>Login</button>
-    </div>
+    <header>
+      <h3>Snap<span>shot</span></h3>
+    </header>
+    <main>
+      <div class='form-group'>
+        <input type='text' v-model='email' placeholder='email'/>
+        <input type='password' v-model='password' placeholder='password'/>
+        <button class='login-btn'>Login</button>
+      </div>
+    </main>
+    <footer>
+      <p>
+        No Account? <router-link class='link' to ='/register'>Sign Up</router-link>
+      </p>
+    </footer>
   </div>
 </template>
 
@@ -23,28 +32,81 @@
 
 <style lang="scss" scoped>
 .login-page {
-  padding: 25px;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  box-sizing: border-box;
 
-  h3 {
-    color: #000;
-    font-size: 28px;
-    text-align: center;
-    font-weight: 900;
-    span {
-      font-weight: 300;
+  header {
+    h3 {
+      color: #171717;
+      font-size: 28px;
+      text-align: center;
+      font-weight: 900;
+      span {
+        font-weight: 300;
+      }
     }
   }
 
   .form-group {
+    flex: 1;
     display: flex;
-    justify-content: center;
+    justify-content: flex-start;
     flex-flow: column;
+    padding: 25px;
+
     input {
       width: 100%;
       height: 30px;
       border: 1px solid #ddd;
       margin-bottom: 15px;
       text-indent: 5px;
+      background: #eee;
+      outline: none;
+
+      &:focus {
+        border: 1px solid #aaa;
+      }
+    }
+
+    button {
+      width: 100%;
+      height: 30px;
+      background: #ffce00;
+      appearance: none;
+      border: none;
+      outline: none;
+      border-radius: 8px;
+
+      color: #171717;
+      font-size: 18px;
+      font-weight: 500;
+    }
+  }
+
+  footer {
+    width: calc(100% - 50px);
+    height: 40px;
+    background-color: #eee;
+    box-shadow: 0px -1px 3px rgba(0, 0, 0, 0.2);
+    padding: 15px 25px;
+    position: fixed;
+    bottom: 0;
+
+    p {
+      color: #888;
+      font-size: 16px;
+      line-height: 40px;
+      margin: 0px;
+      padding: 0px;
+      text-align: center;
+    }
+
+    .link {
+      color: #ffce00;
+      text-decoration: none;
+      font-weight: 700;
     }
   }
 }
