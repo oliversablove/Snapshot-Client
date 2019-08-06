c5db0bdb5a3c2398f4625b683f69c24099ef34de
oliversablove <osablove@gmail.com>
2019-08-06 14:36:54 -0400

    Create Home, Login, and Register Vues
    -Created vues for established routes - home, login, register.
    -Set up router for Vues in router.js.
    -Add CSS (Sass) to App Vue (global vue) and login Vue.

--- src/router.js
+++ src/router.js
@@ -1,25 +1,56 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Login from './views/Login.vue'
+import Register from './views/Register.vue'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: {
+        requiresAuth: false
+      }
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/register',
+      name: 'register',
+      component: Register,
+      meta: {
+        requiresAuth: false
+      }
+    },
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 })
+
+// Checks auth to protect from getting to requiresAuth routes if not logged in (redirects to login)
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (localStorage.getItem('jwt') == null) {
+      next({
+        path: '/login',
+        params: { nextUrl: to.fullPath }
+      })
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
+})
+
+export default router
---
+++ src/views/Login.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class='login-page'>
+    <h3>Snap<span>shot</span></h3>
+    <div class='form-group'>
+      <input type='text' v-model='email' placeholder='email'/>
+      <input type='password' v-model='password' placeholder='password'/>
+      <button class='login-btn'>Login</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'login',
+    data () {
+      return {
+        email: '',
+        password: ''
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+.login-page {
+  padding: 25px;
+
+  h3 {
+    color: #000;
+    font-size: 28px;
+    text-align: center;
+    font-weight: 900;
+    span {
+      font-weight: 300;
+    }
+  }
+
+  .form-group {
+    display: flex;
+    justify-content: center;
+    flex-flow: column;
+    input {
+      width: 100%;
+      height: 30px;
+      border: 1px solid #ddd;
+      margin-bottom: 15px;
+      text-indent: 5px;
+    }
+  }
+}
+</style>
