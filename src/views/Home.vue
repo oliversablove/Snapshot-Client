f415cd0ccaee569ffd4c6ac3789f1d8ba706e1e7
oliversablove <osablove@gmail.com>
2019-08-06 17:09:33 -0400

    Add Dev Sign Out

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="home">
     Hello, World duuuh!
+    <button @click='logout'>Logout</button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    logout () {
+      localStorage.removeItem('jwt')
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
