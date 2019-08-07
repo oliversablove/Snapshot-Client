2aa0dc39c6fa10a38eeb9c1c72b81b39a680267a
oliversablove <osablove@gmail.com>
2019-08-07 00:30:50 -0400

    Add Image Capture Functionaliy

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,7 +1,14 @@
 <template>
   <main class ="view post">
     <section class="stream">
-      <video ref="video" id="video" width="100%" height="300px" autoplay></video>
+      <video ref="video" id="video" width="100%" height="300" autoplay v-if="!captured"></video>
+      <button class="capture-btn" @click="capture">Capture</button>
+    </section>
+    <section class="capture">
+      <canvas ref="canvas" id="canvas" width="100%" height="300" :class="(captured) ? 'show' : 'hide'"></canvas>
+    </section>
+    <section class="image">
+      <img :src="cap" alt="">
     </section>
   </main>
 </template>
@@ -11,14 +18,29 @@
 export default {
   data () {
     return {
-      video: {}
+      video: {},
+      canvas: {},
+      cap: "",
+      captured: false
     }
   },
-  methods: {},
+  methods: {
+    capture () {
+      let context = this.canvas.getContext('2d').drawImage(this.video, 0, 0, this.canvas.width,
+      this.canvas.height)
+      this.cap = canvas.toDataURL("image/png")
+    }
+  },
   mounted () {
     this.video = this.$refs.video
     this.video.width = window.innerWidth
     this.video.height = window.innerHeight - 80
+
+    if (this.$refs.canvas) {
+      this.canvas = this.$refs.canvas
+      this.canvas.width = window.innerWidth
+      this.canvas.height = window.innerHeight - 80
+    }
 
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices.getUserMedia({
@@ -33,3 +55,18 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.show {
+  display: block;
+}
+.hide {
+  display: none;
+}
+.capture-btn {
+  position: absolute;
+  left: 50%;
+  bottom: 65px;
+  transform: translateX(-50%);
+}
+</style>
