9d85f91a97c381c34233e5b73d37dca8eaf85868
oliversablove <osablove@gmail.com>
2019-08-06 23:37:53 -0400

    Add Feed and Post Styling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,8 @@
         <img class="post-picture" :src="post.image" :alt="post.desc" />
       </section>
       <footer class="post-desc">
-        {{ post.display_name + ': ' + post.desc }}
+        <p><strong>{{ post.display_name }}</strong> {{ post.desc }}</p>
+        <p class="timestamp">{{ timestampToDate(post.timestamp) }}</p>
       </footer>
     </article>
 
@@ -30,6 +31,19 @@ export default {
       // previous code (supposed to be global in store.js mutations)
       localStorage.removeItem('jwt')
       this.$router.push('/login')
+    },
+    timestampToDate (timestamp) {
+      let d = new Date(timestamp)
+      let year = d.getFullYear()
+      let month = d.getMonth() + 1
+      if (month < 10) {
+        month = "0" + month
+      }
+      let day = d.getDate()
+      if (day < 10) {
+        day = "0" + day
+      }
+      return month + '/' + day + '/' + year
     }
   }
 }
