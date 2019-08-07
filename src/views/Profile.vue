613a524fd9ede6fedb247606fee852d30a508e9a
oliversablove <osablove@gmail.com>
2019-08-07 02:58:16 -0400

    Add Profile Vue

---
+++ src/views/Profile.vue
@@ -0,0 +1,29 @@
+<template>
+  <main class="view profile">
+
+    <section class="profile-head">
+      {{ display_name }}
+    </section>
+
+    <section class="posts">
+      <div class="post" v-for="post in posts" :key="post._id">
+
+        <img :src="post.image" :alt="post.desc" c;ass="post-image" />
+
+      </div>
+    </section>
+
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'profile',
+  data () {
+    return {
+      display_name: '',
+      posts: []
+    }
+  }
+}
+</script>
