934540739fef99c236f8d7c32d300a453fa22f16
oliversablove <osablove@gmail.com>
2019-08-07 11:03:20 -0400

    Update Icons

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,8 +2,8 @@
   <footer class="footer">
     <button @click="goto('/')"><i class="material-icons">home</i></button>
     <button><i class="material-icons">search</i></button>
-    <button @click="goto('/newpost')"><i class="material-icons">camera</i></button>
-    <button><i class="material-icons">announcement</i></button>
+    <button @click="goto('/newpost')"><i class="material-icons">add_box</i></button>
+    <button><i class="material-icons">favorite_border</i></button>
     <button @click="goto('/profile')"><i class="material-icons">account_box</i></button>
   </footer>
 </template>
