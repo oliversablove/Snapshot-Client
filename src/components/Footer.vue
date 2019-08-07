21f2c6e1a24ce5399f8aadc83f4f121fcd85cee3
oliversablove <osablove@gmail.com>
2019-08-07 12:22:00 -0400

    Comment-out unuseable buttons

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
   <footer class="footer">
     <button @click="goto('/')"><i class="material-icons">home</i></button>
-    <button><i class="material-icons">search</i></button>
+    <!-- <button><i class="material-icons">search</i></button> -->
     <button @click="goto('/newpost')"><i class="material-icons">add_box</i></button>
-    <button><i class="material-icons">favorite_border</i></button>
+    <!-- <button><i class="material-icons">favorite_border</i></button> -->
     <button @click="goto('/profile')"><i class="material-icons">account_box</i></button>
   </footer>
 </template>
